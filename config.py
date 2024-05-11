dbcb51a307325e76387775a7e9e88a8e2f57ed2f
Akash <158745842+Akash8t2@users.noreply.github.com>
2024-05-11 22:46:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Akash8t2/Akking-music",
+    "https://github.com/Akash8t2/CYBERCOBRAS69",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
